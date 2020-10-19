47d8c4de0259157d93e2d69e5b4de857aae7e737
Swapnil-Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-10-19 06:46:50 +0530

    added avatar image

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ class Sidebar extends Component {
       <div id="sidebar">
         <div className="user-profile">
           <div className="avatar">
-            <img src="https://www.flaticon.com/svg/static/icons/svg/2919/2919600.svg" />
+            <img src={auth.currentUser.photoURL} />
           </div>
           <div>{auth.currentUser.displayName}</div>
           <div
